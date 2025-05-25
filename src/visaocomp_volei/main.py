7f8ced6f5079568884c7b9a1bbe76378eb604b72
thalia <thaliaaxavier2022@gmail.com>
2025-05-25 11:33:13 -0300

    base projeto

---
+++ src/visaocomp_volei/main.py
@@ -0,0 +1,16 @@
+import sys
+from utils import parse_args
+
+def main():
+     args = parse_args()
+     try:
+        problema = args.problema
+        print(f"Resolvendo o problema: {problema}") 
+        return 0
+     except AttributeError:
+        print("Erro: {e}")
+        return 1
+     
+
+if __name__ == "__main__":
+    sys.exit(main())
